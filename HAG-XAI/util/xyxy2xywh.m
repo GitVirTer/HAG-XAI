33e81624124ea4d02cc1f902a687b1de2301d2f7
GitVirTer <36156907+GitVirTer@users.noreply.github.com>
2023-12-16 00:51:00 +0800

    Delete HAG-XAI directory

--- HAG-XAI/util/xyxy2xywh.m
+++
@@ -1,8 +0,0 @@
-function p_new = xyxy2xywh(p)
-    p_new = zeros(size(p),"like",p);
-    p_new(:,1) = (p(:,1)+p(:,3))./2;
-    p_new(:,2) = (p(:,2)+p(:,4))./2;
-    p_new(:,3) = p(:,3)-p(:,1);
-    p_new(:,4) = p(:,4)-p(:,2);
-
-end
